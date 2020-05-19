adc17052251ca9a4eb67a72fd2abd2092e06d140
bhuwan.sharma <bhuwan.sharma@kane-group.com>
2020-05-19 17:38:02 +0530

    initialize materialize UI design for webapp

--- src/main/java/com/bhuwan/components/Layout.java
+++ src/main/java/com/bhuwan/components/Layout.java
@@ -1,4 +1,12 @@
 package com.bhuwan.components;
 
+import org.apache.tapestry5.annotations.Import;
+
+@Import(stylesheet =
+        {"context:/assets/css/materialize.css",
+                "context:/assets/css/materialize.min.css",
+                "context:/assets/js/materialize.js",
+                "context:/assets/js/materialize.min.js"
+        })
 public class Layout {
 }
